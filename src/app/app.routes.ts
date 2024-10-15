6af7399e16f1fb920370d9b2ff21ba580214dc4e
Tania024 <84852408+Tania024@users.noreply.github.com>
2024-10-14 23:05:32 -0500

    Clase del lunes 14 de octubre

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { AcercaDeComponent } from './pages/acerca-de/acerca-de.component';
+import { MisionComponent } from './pages/mision/mision.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    
+    {path: 'Inicio', title: 'Inicio', component: InicioComponent, },
+    {path: 'Acercade', title: 'Acercade', component: AcercaDeComponent},
+    {path: 'Mision', title: 'Mision', component: MisionComponent},
+
+];
